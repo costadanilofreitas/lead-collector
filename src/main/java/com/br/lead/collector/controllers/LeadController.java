896b1dfae17632c913bc60606ffbd514e4472d16
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-05-11 17:14:30 -0300

    Criar metodo para regatar um lead pelo indice

--- src/main/java/com/br/lead/collector/controllers/LeadController.java
+++ src/main/java/com/br/lead/collector/controllers/LeadController.java
@@ -1,14 +1,21 @@
 package com.br.lead.collector.controllers;
 
+import com.br.lead.collector.models.Lead;
 import com.br.lead.collector.services.LeadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/lead")
 public class LeadController {
 
     @Autowired
     private LeadService leadService;
 
-    
+    @GetMapping("/{indice}")
+    public Lead buscarLead(@PathVariable Integer indice){
+        Lead lead = leadService.buscarPorIndice(indice);
+        return lead;
+    }
+
 }
